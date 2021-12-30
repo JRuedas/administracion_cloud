ff20817d31f58817cc976960e5271a1a19c9d6b8
JRuedas <11670560+JRuedas@users.noreply.github.com>
2021-12-30 21:38:03 +0100

    Fixes problem

--- actividad1/script5.sh
+++ actividad1/script5.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 
-CURRENT_DIR=$PWD
 PREFIX=$(date +%Y%m%d)
 
-echo "Renaming all jpg/jpeg files in $CURRENT_DIR"
+if [ -z "$(ls -A $PWD/*.jp*g 2>/dev/null)" ]; then
+    echo "No hay ficheros JPG/JPEG en el directorio"
+    exit 1
+fi
 
-for file in $CURRENT_DIR/*.jp*g; do
-    NEW_FILE="$CURRENT_DIR/$PREFIX-${file##*/}"
+echo "Renombrando los ficheros JPG/JPEG de $PWD"
+
+for file in $PWD/*.jp*g; do
+    NEW_FILE="$PWD/$PREFIX-${file##*/}"
     mv $file $NEW_FILE
 done
