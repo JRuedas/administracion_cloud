0ce186b45c3bf62f9a355676ea851241836e5015
JRuedas <11670560+JRuedas@users.noreply.github.com>
2022-01-04 20:11:28 +0100

    Improved script 3

--- actividad1/script3.sh
+++ actividad1/script3.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
+
 set -e
 
-NUM_ARGS=$#
-
 echo "$0"
-echo "$NUM_ARGS"
+echo "$#"
 
 TEXT=""
 
-for i in {1..2}; do
-    if [ ! -z "$1" ]; then
-        TEXT="$TEXT $1"
-        shift
-    fi
+for ((i = 1; i <= $#; i++ )); do
+  if [[ $i -lt 3 ]]; then
+    TEXT="$TEXT ${!i}"
+  else
+    TEXT="$TEXT \n${!i}"
+  fi
 done
 
-if [ -z "$TEXT" ]; then
-    exit 0 # TODO: Should return error (non-zero) or zero? Depends if no arguments can be considered an error
-fi
-
-echo $TEXT
-
-if [ $NUM_ARGS -gt 2 ]; then
-    for arg in "$@"; do
-        echo $arg
-    done
-fi
+echo -e $TEXT
